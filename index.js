98790619cea5cbbba8543b6650a88b6692dbdc46
FAISSOIL Ali <75081701+Faissoil22@users.noreply.github.com>
2022-03-16 17:41:44 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,155 @@
+
+function Page() {
+    return (
+        <div>
+            <header>
+                <nav>
+                    <img src="./javascript-logo.png" width="50px" />
+                    <img src="./react-logo.png" width="100px" />
+                </nav>
+            </header>
+            <h1>Mes connaissances et compétences </h1>
+            <p>
+                <strong>Qualification logicielle</strong>
+            </p>
+            <ul>
+                <li>
+                    <li>Etude et analyse des besoins techniques et fonctionnels</li>
+                    <li>Evaluation des exigences utilisateurs</li>
+                    <li>Rédaction des scénarios et des plans de tests (classiques et BDD)</li>
+                    <li>Exécution des campagnes de tests fonctionnels </li>
+                    <li>Exploration des bases de données </li>
+                    <li>Proposition et mise en œuvre des actions préventives et correctives</li>
+                    <li>Déclaration et suivi des anomalies </li>
+                    <li>Support et formation aux utilisateurs</li>
+                    <li>Rédaction du PV de recette et bilan de tests</li>
+                    <li>Rédaction de rapports de suivi d’activité </li>
+                    <li>Rédaction de cahier de charges</li>
+                    <li>Estimation des coûts et risques liés</li>
+                    <li>Elaboration et suivi de planning</li>
+                    <li>Participation aux rituels agiles</li>
+                </li>
+            </ul>
+            <p>
+                <strong>Compétences techniques</strong>
+            </p>
+            <ul>
+                <li>
+                    <li>Méthodologie Agile, Cycle en V</li>
+                    <li>Squash TM, HP Quality Center, JIRA, Mantis BT </li>
+                    <li>Katalon Studio, Ready API, Postman</li>
+                    <li>Langages HTML, CSS, JavaScript, SQL, Shell</li>
+                    <li>SGBD, MySQL, ORACLE, SQL </li>
+                    <li>Pack Office, GANTT</li>
+                    <li>Systèmes Windows, Linux (Shell)</li>
+                </li>
+            </ul>
+            <p>
+                <strong>Compétences métier</strong>
+            </p>
+
+            <ul>
+                <li>Banque (crédit à la consommation), Commerce </li>
+            </ul>
+
+            <h2>Formations</h2>
+            <ul>
+                <li>2022 EQL ATOLL          Certification ISTQB Niveau Fondation (82.5%) </li>
+                <li>2021 OpenClassRooms     Formation Développeur Web </li>
+                <li>2011 Université Paris 7 Master 1 des Métiers d’étude </li>
+                <li>2010 Univeristé Paris 7 Diplôme de licence Droit, Économie et Gestion </li>
+                <li>Anglais                 Niveau B2</li>
+            </ul>
+
+            <h2>Expériences professionnelles</h2>
+            <ul>
+                <li>
+                    <p>12/2021 À 03/2022</p>
+                    <p><strong>CLIENT : FORMATION QUALIFICATION LOGICIELLE Ecole QL</strong></p>   
+                    
+                    <p>Objectif : Consolider mes connaissances en méthodes Qualification Logicielle et obtenir au terme de la formation la Certification “ISTQB Fondation”</p>
+                    <li>Savoir recueillir et analyser le besoin client </li>
+                    <li> Lire et analyser des spécifications</li>
+                    <li> Élaborer la stratégie et organisation de la recette</li>
+                    <li>Concevoir de tests et de jeux de données </li>
+                    <li>Exécuter une campagne de tests avec SQUASH TM</li>
+                    <li>Ecrire des requêtes SQL</li>
+                    <li>Définir et gérer les environnements de tests</li>
+                    <li>Automatiser des tests avec l’outil KATALON STUDIO </li>
+                    <li>Déclarer des anomalies sur MANTIS BT et sur JIRA</li>
+                    <li>Savoir piloter la recette (avancement, risques, anomalies) et définir les critères de Go/NoGo </li>
+                    <li>Réaliser les tests fonctionnels de Web Services (Ready API) </li>
+                    <li>Travailler en Agile Scrum et en Cycle en V</li>
+                    <p>Projets :</p> Au sein d’une équipe de 3-4 personnes mes projets consistaient à:
+                    <li>Assurer la qualité d’une application de gestion des activités d’une école</li>
+                    <li>Assurer la qualité d'un traitement automatique (batch) pour la gestion des absences d’une cantine </li>
+                    <li>Mettre en place une stratégie de recette cycle en V pour une application de gestion d’avions au sol </li>
+                    <li>Mettre en place une stratégie de recette Agile scrum pour une application de gestion de jardins partagés</li>
+                    <p>Méthodologie de travail : Agile SCRUM AMOA</p>
+                    <li>Analyser les expressions de besoin du client</li>
+                    <li>Rédiger les users stories et epics </li>
+                    <li>Constituer le Backlog produit via JIRA </li>
+                    <p>Test et Recette: </p>
+                    <li>Définir la stratégie de tests à partir des Users Stories</li>
+                    <li>Rédiger les Scénarios et cas de test sur SQUASH TM</li>
+                    <li>Exécuter les tests fonctionnels et techniques </li>
+                    <li>Identifier et déclarer les anomalies sur JIRA</li>
+                    <li>Rédiger le reporting journalier (Attendu/réalisé, Pourcentage de couverture des exigences, Bilan) </li>
+                    <p>Environnement technique : Squash TM, JIRA, Trello, SQL, MySQL, Gantt</p>
+                </li>
+                <li>
+                    <p>02/2021 À 09/2021</p> 
+                    <p><strong>CLIENT: FORMATION DEVELOPPEUR WEB OpenClassRooms</strong></p> 
+                    <p>Contexte :</p> 
+                    <p>Ma mission consistait à apprendre à créer des sites web responsives avec les langages HTML, CSS et Javascript </p>
+                    <p>Réalisation(s) :</p> 
+                    <li>Intégrer une maquette en respectant la récommandation W3C</li>  
+                    <li>Dynamiser un site web SEO </li> 
+                    <li>Créer un site mobile first </li>
+                    <li>Créer un site de e-commerce avec une API REST sécurisée </li>
+                    <li>Créer un réseau social avec vue.JS React.Js</li> 
+                    
+                    <p>Environnement : Visual Studio code, GitHub</p>
+                </li>
+                <li>
+                    <p>08/2020 À 01/2021</p> 
+                    <p>CLIENT : MCDONALD’S FRANCE</p> 
+                    <p>FONCTION : CHARGE DE RECETTE</p> 
+                    <p>Contexte : Ma mission consistait à configurer et à tester les équipements de prise des commandes (Caisses, Bornes et Click and collect, Deliveroo, Uber Eats)</p> 
+                    <p>Réalisation(s) :</p> 
+                    <li>Mettre en place les packages des campagnes marketing des produits McDonald’s destinés aux 1485 restaurants en France</li>
+                     <li>Exécuter les tests fonctionnels sur la prise, le paiement et l’affichage des commandes sur les différents équipements</li> 
+                     <li>Configurer les équipements destinés aux restaurants et Réaliser les tests techniques afin de valider leur déploiement</li> 
+                     <li>Réaliser les migrations des bases de données des modules de gestion internes et réaliser les tests fonctionnels </li>
+                     <p>Environnement : Windows, Machine Virtuelle, Windows server</p>
+                </li>
+                <li>
+                    <p>04/2018 À 06/2020 </p>
+                    <p><strong>CLENT: CREDIT AGRICOLE CONSUMER FINANCE</strong></p>
+                    <p>FONCTION : CHARGE DE RECETTE </p>
+                    <p>Contexte : Ma mission consistait à participer à la recette fonctionnelle en mode Agile du projet DIGONCONSO 2.0</p>
+                     <p>Une refonte des offres multicanales de crédits à la consommation </p>
+                     <p>Réalisation(s) :</p> 
+                    <li>Faire la revue des User Stories</li> 
+                    <li>Concevoir des cas de test avec Squash </li> 
+                    <li>Exécution des cas de test sur les environnements de Test Parcours Client et Conseiller </li> 
+                    <li>Déclarer et suivre les anomalies sur JIRA et Mantis BT · Participer à l’ouverture des tickets aux UAT (testsd’acceptance utilisateurs)</li>  
+                    <li>Réaliser un reporting hebdomadaire sur le statut des anomalies en cours de correction </li> 
+                    <li> Former un nouvel arrivant </li>
+                    <p>Environnement : JIRA, Squash TM, Windows, Web, Android, IOS, Microsoft Office</p>
+                </li>
+                <li>
+                    <p>09/2012 À 10/2017</p> 
+                    <p>CLIENT: LAGARDERE ENTERTAINEMENT </p>
+                    <p>FONCTION : VENDEUR EN LIBRAIRIE </p>
+                    <p>Contexte : Ma mission consistait à accueillir les clients, Tenir la caisse, assurer les dépôts bancaires, gérer les stocks, commander les livres et gérer les retours invendus </p>
+                    <p>Réalisation(s) :</p> <li>Organiser les cessions de dédicaces des auteurs (David Foenkinos, Sonia Rolland, Douglas Kennedy, Le professeur Chabrol, Mémouna Hintermann, Alain Mabanckou, Jean Louis Debré, Bernard Werber) Environnement : JIRA, Squash TM, Windows, Web, Android, IOS, Microsoft Office</li>
+                </li>
+                <footer>
+                    <p><small>(c) Ali Faissoil CV 2022. Tous droits Réservés </small></p>
+                </footer>
+            </ul>
+        </div>
+    );
+}
+ReactDOM.render(<Page />, document.getElementById("root"));
